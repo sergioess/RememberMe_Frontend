2d19651bb3bc453d38e48d3226f82f1940bbfbde
Sergioess <sergioess@hotmail.com>
2021-11-30 19:59:57 -0500

    liat notifications

---
+++ src/app/notifications/list-notifications/list-notifications.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list-notifications',
+  templateUrl: './list-notifications.component.html',
+  styleUrls: ['./list-notifications.component.css']
+})
+export class ListNotificationsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/notifications/notifications.module.ts
+++ src/app/notifications/notifications.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AngularMaterialModule } from '../angular-material/angular-material.module';
+import { ListNotificationsComponent } from './list-notifications/list-notifications.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ListNotificationsComponent
+  ],
   imports: [
     CommonModule,
     AngularMaterialModule,
